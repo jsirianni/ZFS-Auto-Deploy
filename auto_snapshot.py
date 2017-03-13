a5a2e43861aa0b0f49c29beb819c81746c3f3295
Joseph Sirianni <firefoxx04a@gmail.com>
2017-03-13 12:07:04 -0400

    Add print statements when snapshots are enabled

--- auto_snapshot.py
+++ auto_snapshot.py
@@ -31,29 +31,34 @@ def enable(zpool_name):
     # frequent snapshots. Every 15min, retain last four snapshots
     if input("\nEnable frequent snapshots (Every 15min, retaining the last four snapshots)? ") == "y":
         os.system("sudo zfs set com.sun:auto-snapshot:frequent=true " + zpool_name)
+        print("Frequent snapshots enabled for " + zpool_name)
     else:
         os.system("sudo zfs set com.sun:auto-snapshot:frequent=false " + zpool_name)
 
     # Hourly snapshots, retain last 24 snapshots
     if input("\nEnable hourly snapshots (Every hour, retaining the last 24 snapshots)? ") == "y":
         os.system("sudo zfs set com.sun:auto-snapshot:hourly=true " + zpool_name)
+        print("Hourly snapshots enabled for " + zpool_name)
     else:
         os.system("sudo zfs set com.sun:auto-snapshot:hourly=false " + zpool_name)
 
     # Daily snapshots, retain last 31 snapshots
     if input("\nEnable daily snapshots (Every day, retaining the last 31 snapshots)? ") == "y":
         os.system("sudo zfs set com.sun:auto-snapshot:daily=true " + zpool_name)
+        print("Daily snapshots enabled for " + zpool_name)
     else:
         os.system("sudo zfs set com.sun:auto-snapshot:daily=false " + zpool_name)
 
     # Weekly snapshots, retain last 7 snapshots
     if input("\nEnable weekly snapshots (Every week, retaining the last 7 snapshots)? ") == "y":
         os.system("sudo zfs set com.sun:auto-snapshot:weekly=true " + zpool_name)
+        print("Weekly snapshots enabled for " + zpool_name)
     else:
         os.system("sudo zfs set com.sun:auto-snapshot:weekly=false " + zpool_name)
 
     # Monthly snapshots, retain last 12 snapshots
     if input("\nEnable monthly snapshots (Every monthly, retaining the last 12 snapshots)? ") == "y":
         os.system("sudo zfs set com.sun:auto-snapshot:monthly=true " + zpool_name)
+        print("Monthly snapshots enabled for " + zpool_name)
     else:
         os.system("sudo zfs set com.sun:auto-snapshot:monthly=false " + zpool_name)
