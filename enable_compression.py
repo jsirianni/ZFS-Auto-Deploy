a99778983699165dca6a3d1468a69434a196cff0
Joseph Sirianni <firefoxx04a@gmail.com>
2017-04-06 22:31:33 -0400

    misc bug fix / spelling

--- enable_compression.py
+++ enable_compression.py
@@ -9,17 +9,17 @@ import os
 
 
 '''
-Call this function to disable zfs compresison
+Call this function to disable zfs compression
 '''
 def disable(zpool_name):
-    os.system("sudo zfs set compresison=off " + zpool_name)
+    os.system("sudo zfs set compression=off " + zpool_name)
 
 
 
 '''
 Call this function to enable zfs compression
-Supported compresison methods: lzjb, lz4, gzip, gzip[1-9], zle
+Supported compression methods: lzjb, lz4, gzip, gzip[1-9], zle
 If no compression type is passed, lz4 will be used
 '''
 def enable(zpool_name, comp_type="lz4"):
-    os.system("sudo zfs set compresison=" + comp_type + " " + zpool_name)
+    os.system("sudo zfs set compression=" + comp_type + " " + zpool_name)
