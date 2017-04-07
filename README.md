b590d9bc2d6c5859e73b1886af1cef4df2f06328
Joseph Sirianni <firefoxx04a@gmail.com>
2017-04-07 15:57:48 -0400

    misc fixes

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Version 2.1.0
 Supported OS
   - Ubuntu 16.04 w/ default kernel 4.4 (Officially tested)
   - Debian / Ubuntu (Not tested, may work)
-  
+
 Prerequisites
   - Package: Sudo, python3
   - Execute as root or sudo user
@@ -18,7 +18,7 @@ Features
     - Enable or disable global snapshots (entire zpool)
     - Enable or disable dataset snapshots, for each dataset.
   - Optional: Setup zfs email alerts
-  - Optional: Enable zfs compression
+  - Optional: Enable zfs compression (LZ4 Only)
     - Default compression is lz4
 
 
@@ -36,5 +36,5 @@ How to install
       - email alerts sending email address
           - this email is what the system uses to SEND alerts
               - you should use a throw away / dumby email because the password is stored in plain text!!!
-              
+
       - It is important to note that the email specified in zed.rc can be a real email address. That is different from the sending email used by gmail-alerts.sh
--- enable_compression.py
+++ enable_compression.py
@@ -11,7 +11,7 @@ import os
 
 def disable(zpool_name):
     '''
-    Call this function to disable zfs compression
+    Call this function to disable zfs compression.
     '''
     os.system("sudo zfs set compression=off " + zpool_name)
 
@@ -19,8 +19,20 @@ def disable(zpool_name):
 
 def enable(zpool_name, comp_type="lz4"):
     '''
-    Call this function to enable zfs compression
-    Supported compression methods: lzjb, lz4, gzip, gzip[1-9], zle
-    If no compression type is passed, lz4 will be used
+    Call this function to enable zfs compression.
     '''
-    os.system("sudo zfs set compression=" + comp_type + " " + zpool_name)
+    os.system("sudo zfs set compression=lz4 " + zpool_name)
+
+
+
+def validate(comp_type):
+    '''
+    Call this function to validate the selected compression type.
+    Supported compression methods: lzjb, lz4, gzip.
+    This function returns a boolean value.
+    '''
+    if comp_type == "lzjb" or "lz4" or "gzip":
+        return True
+
+    if comp_type == "off" or ""
+        return False
